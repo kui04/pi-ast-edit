5e0e055224e12510e1b405039d430ed5af79ff09
kui04 <likuiandmc2004@gmail.com>
2026-09-11 17:41:41 +0800

    release: nix static builds for Linux, rustup stays for win/mac

--- flake.nix
+++ flake.nix
@@ -24,26 +24,36 @@
         rustToolchain = pkgs.rust-bin.stable.latest.default.override {
           extensions = [ "rust-src" ];
         };
+        src = pkgs.lib.cleanSourceWith {
+          src = ./.;
+          filter =
+            path: type:
+            type != "directory"
+            || !(builtins.elem (baseNameOf path) [
+              "target"
+              "node_modules"
+              "result"
+            ]);
+        };
+        # Dynamic glibc build (dev / local use).
         piAstEdit = pkgs.rustPlatform.buildRustPackage {
           pname = "pi-ast-edit";
           version = "0.1.0";
-          src = pkgs.lib.cleanSourceWith {
-            src = ./.;
-            filter =
-              path: type:
-              type != "directory"
-              || !(builtins.elem (baseNameOf path) [
-                "target"
-                "node_modules"
-                "result"
-              ]);
-          };
+          inherit src;
+          cargoLock.lockFile = ./Cargo.lock;
+        };
+        # Fully static musl build (release binaries for Linux).
+        piAstEditStatic = pkgs.pkgsStatic.rustPlatform.buildRustPackage {
+          pname = "pi-ast-edit";
+          version = "0.1.0";
+          inherit src;
           cargoLock.lockFile = ./Cargo.lock;
         };
       in
       {
         packages.default = piAstEdit;
         packages.pi-ast-edit = piAstEdit;
+        packages.pi-ast-edit-static = piAstEditStatic;
 
         devShells.default = pkgs.mkShell {
           packages = [
